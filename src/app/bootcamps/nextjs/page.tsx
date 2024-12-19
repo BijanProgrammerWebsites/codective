45b2f40bbd41974e991ac85588d47704793e957a
BijanProgrammer <bijaneisapour@gmail.com>
2024-12-19 12:27:51 +0330

    [ADD] props to hero component

--- src/app/bootcamps/nextjs/page.tsx
+++ src/app/bootcamps/nextjs/page.tsx
@@ -1,12 +1,14 @@
 import HeroComponent from "@/components/hero/hero.component";
 import FeaturesComponent from "@/components/features/features.component";
 
+import NextLogo from "@/logo/next.logo";
+
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <div className={styles.page}>
-      <HeroComponent />
+      <HeroComponent subject="Next.js" logo={<NextLogo />} />
       <FeaturesComponent />
     </div>
   );
--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -2,25 +2,32 @@ import { ReactElement } from "react";
 
 import Image from "next/image";
 
-import heroImage from "@/assets/images/hero.png";
+import dishesImage from "@/assets/images/dishes.png";
 
 import styles from "./hero.module.css";
 
-export default function HeroComponent(): ReactElement {
+type Props = {
+  subject: string;
+  logo: ReactElement;
+};
+
+export default function HeroComponent({ subject, logo }: Props): ReactElement {
   return (
     <section id="intro" className={styles.hero}>
       <div className={styles.writings}>
         <h1>
           <span className={styles.subtitle}>فقط تو 2 ماه</span>
           <span className={styles.title}>
-            <span className={styles.highlight}>Next.js</span>
+            <span className={styles.highlight}>{subject}</span>
             رو قورت بده!
           </span>
         </h1>
         <div className={styles.description}>
           <p>
-            تو این بوت‌کمپ، Next.js رو از پایه یاد می‌گیریم و یه سایت کامل توسعه
-            میدیم. هر هفته دو جلسه تدریس آنلاین و یک جلسه رفع اشکال داریم.
+            تو این بوت‌کمپ،
+            {subject}
+            رو از پایه یاد می‌گیریم و یه سایت کامل توسعه میدیم. هر هفته دو جلسه
+            تدریس آنلاین و یک جلسه رفع اشکال داریم.
           </p>
           <p>
             از همه مهم‌تر بهتون تسک داده میشه و Code Review داریم. همچنین تو
@@ -32,7 +39,14 @@ export default function HeroComponent(): ReactElement {
         </div>
       </div>
       <div className={styles.visuals}>
-        <Image src={heroImage} alt="" height={300} loading="eager" />
+        <Image
+          className={styles.dishes}
+          src={dishesImage}
+          alt=""
+          height={300}
+          loading="eager"
+        />
+        {logo}
       </div>
     </section>
   );
