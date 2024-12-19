a3e542e84bbc1c3ff33ceed192fa6433f1fd5af9
BijanProgrammer <bijaneisapour@gmail.com>
2024-12-19 13:29:50 +0330

    [ADD] features

--- src/components/features/features.component.tsx
+++ src/components/features/features.component.tsx
@@ -3,5 +3,23 @@ import { ReactElement } from "react";
 import styles from "./features.module.css";
 
 export default function FeaturesComponent(): ReactElement {
-  return <section id="features" className={styles.features}></section>;
+  return (
+    <section id="features" className={styles.features}>
+      <h2>ویژگی‌ها</h2>
+      <ul>
+        <li>مدت دوره: 8 هفته</li>
+        <li>2 جلسه تدریس آنلاین در هفته</li>
+        <li>1 جلسه رفع اشکال در هفته</li>
+        <li>ضبط و انتشار جلسات برای غایبین</li>
+        <li>برنامه‌ی مشخص</li>
+        <li>نقشه‌ی راه شفاف</li>
+        <li>کاملاً پروژه‌محور و تسک‌محور</li>
+        <li>پشتیبانی اختصاصی</li>
+        <li>گروه خصوصی</li>
+        <li>منتورینگ حرفه‌ای</li>
+        <li>بررسی دقیق کدها</li>
+        <li>پروژه‌های قابل‌ارائه در رزومه</li>
+      </ul>
+    </section>
+  );
 }
