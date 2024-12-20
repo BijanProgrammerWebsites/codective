09ff54abeb7672a9fc190c402996d2e028dfcc09
BijanProgrammer <bijaneisapour@gmail.com>
2024-12-20 17:28:11 +0330

    [RESPONSIVE] hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Vazirmatn } from "next/font/google";
 import "@/styles/background.css";
 import "@/styles/button.css";
 import "@/styles/palette.css";
+import "@/styles/typography.css";
 
 import "./globals.css";
 
